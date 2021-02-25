bf76749962a60df2f3766f4622ffd93a0ebdc3ad
blustro <brunolustro@gmail.com>
2021-02-25 17:59:00 +0000

    Added Angular Universal

--- recipe-app/src/app/app.component.ts
+++ recipe-app/src/app/app.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as fromApp from './store/app.reducer';
 import * as AuthActions from './auth/store/auth.actions';
+import { isPlatformBrowser } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  constructor(private store: Store<fromApp.AppState>) {}
+  constructor(
+    private store: Store<fromApp.AppState>,
+    @Inject(PLATFORM_ID) private platformId
+  ) {}
 
   ngOnInit() {
-    this.store.dispatch(new AuthActions.AutoLogin());
+    if (isPlatformBrowser(this.platformId))
+      this.store.dispatch(new AuthActions.AutoLogin());
   }
 }
