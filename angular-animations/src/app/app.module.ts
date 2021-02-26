ae79fb0ae2165eca8dd5e694f48e43f11c54b1d8
blustro <brunolustro@gmail.com>
2021-02-26 18:01:44 +0000

    Angular Animations added

---
+++ angular-animations/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
